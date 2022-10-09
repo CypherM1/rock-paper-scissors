07f340200b9fbe166e0cd2e853e837f49e019985
Justin Sears <justinrsears@gmail.com>
2022-10-08 18:05:26 -0700

    added pseudocode to brains.js

---
+++ brains.js
@@ -0,0 +1,13 @@
+//Prompt the user to type in Rock, Paper, or Scissors. Make case-insensitive
+//playerSelection()...... use prompt()
+
+//Have Computer pick Rock, Paper, or Scissors at random.
+//computerSelection()
+
+//Take playerSelection and computerSelection and determine who won.
+//game()
+
+//Play a 5 round game that keeps score and reports a winner or loser at the end.
+//playRound()...... use console.log() to display winner or loser.
+
+console.log("Hello World");
