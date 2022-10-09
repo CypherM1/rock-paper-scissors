baf6b6a505f2e6d623b27c0d3235e1d86ea1749b
Justin Sears <justinrsears@gmail.com>
2022-10-08 19:51:30 -0700

    I was able to get the variables set and playRound working

--- brains.js
+++ brains.js
@@ -1,13 +1,73 @@
 //Prompt the user to type in Rock, Paper, or Scissors. Make case-insensitive
-//playerSelection()...... use prompt()
+//use prompt()
+
+const playerPrompt = prompt("Type in Rock, Paper, or Scissors");
+
+//convert playerPromt to all lower case.
+//Check that user chose Rock, Paper, or Scissors.
+//Set Variable playerSelection().
+const playerSelection = answerCheck();
+
+function answerCheck() {
+    const  setLowerCase = playerPrompt.toLowerCase();
+    if (setLowerCase === "rock" || setLowerCase === "paper" || setLowerCase === "scissors") {
+        console.log("Let us see who wins...")
+    } else {
+        alert("That was not one of the options. Refresh the page and try again");
+    }
+    return setLowerCase;
+};
 
 //Have Computer pick Rock, Paper, or Scissors at random.
-//computerSelection()
+//Set Variable computerSelection()
+
+const computerSelection = getRandomChoice();
+console.log(computerSelection);
+
+function getRandomChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
+    switch (randomNum) {
+      case 0:
+        return 'rock';
+      case 1:
+        return 'paper';
+      case 2:
+        return 'scissors';
+    };
+  };
 
 //Take playerSelection and computerSelection and determine who won.
-//game()
+//playRound()
+
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = " ";
+
+playRound(playerSelection, computerSelection);
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        roundWinner = "tie";
+    }
+    if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
+    ) {
+        playerScore++
+        roundWinner = "player";
+    }
+    if (
+        (computerSelection === "rock" && playerSelection === "scissors") ||
+        (computerSelection === "scissors" && playerSelection === "paper") ||
+        (computerSelection === "paper" && playerSelection === "rock")
+      ) {
+        computerScore++;
+        roundWinner = "computer";
+      }
+}
+
+console.log(playerScore, computerScore, roundWinner);
 
 //Play a 5 round game that keeps score and reports a winner or loser at the end.
-//playRound()...... use console.log() to display winner or loser.
-
-console.log("Hello World");
+//game()...... use console.log() to display winner or loser.
